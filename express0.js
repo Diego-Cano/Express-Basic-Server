045817800a51ea2a14f6d7769b0b345b042cd556
Diego-Cano <64940291+Diego-Cano@users.noreply.github.com>
2024-01-23 20:39:10 -0800

    Express app
    Basic server using express

---
+++ express0.js
@@ -0,0 +1,30 @@
+//required import
+import express from 'express';
+//setup 
+const app = express();
+app.set('port', process.env.PORT || 3000);
+//home routr
+app.get('/', (req, res) =>{
+    res.type('text/plain');
+    res.send("Hello World");
+});
+//about route
+app.get('/about', (req, res) =>{
+    res.type('text/plain');
+    res.send("About Page");
+});
+
+//404 message
+app.use((req, res) =>{
+    res.type('text/plain');
+    res.status(404).send('404 - Not Found');
+});
+
+// Setting up the server 
+const PORT = process.env.PORT || 3000;
+
+
+// Starting the server and logging a message
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
